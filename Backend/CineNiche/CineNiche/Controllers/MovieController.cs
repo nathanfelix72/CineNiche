875cfbec9f6ed1ed9a9b8ac8e03861772296514b
Nathan Felix <nathan.felix@gmail.com>
2025-04-08 20:57:29 -0600

    trying to get the error again

--- Backend/CineNiche/CineNiche/Controllers/MovieController.cs
+++ Backend/CineNiche/CineNiche/Controllers/MovieController.cs
@@ -9,6 +9,7 @@ namespace CineNiche.Controllers
 {
     [Route("[controller]")]
     [ApiController]
+    [Authorize(Roles = "Administrator")]
     public class MovieController : ControllerBase
     {
         private MoviesContext _moviesContext;
@@ -76,7 +77,6 @@ namespace CineNiche.Controllers
         }
         
         [HttpPost("AddMovie")]
-        [Authorize(Roles = "Administrator")]
         public IActionResult AddProject([FromBody] MoviesTitle newMovie)
         {
             _moviesContext.MoviesTitles.Add(newMovie);
@@ -85,7 +85,6 @@ namespace CineNiche.Controllers
         }
 
         [HttpPut("UpdateMovie/{showId}")]
-        [Authorize(Roles = "Administrator")]
         public IActionResult UpdateMovie(int showId, [FromBody] MoviesTitle updatedMovie)
         {
             var existingMovie = _moviesContext.MoviesTitles.Find(showId);
@@ -143,7 +142,6 @@ namespace CineNiche.Controllers
         }
 
         [HttpDelete("DeleteMovie/{showId}")]
-        [Authorize(Roles = "Administrator")]
         public IActionResult DeleteMovie(int showId)
         {
             var movie = _moviesContext.MoviesTitles.Find(showId);
