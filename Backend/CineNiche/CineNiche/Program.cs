25db933591a8090fc980e098b25f95167dd65bcf
Nathan Felix <nathan.felix@gmail.com>
2025-04-10 10:29:05 -0600

    weirdness

--- Backend/CineNiche/CineNiche/Program.cs
+++ Backend/CineNiche/CineNiche/Program.cs
@@ -75,7 +75,7 @@ builder.Services.AddCors(options =>
 // Add HttpClient for Python recommender microservice
 builder.Services.AddHttpClient<MovieRecommenderService>(client =>
 {
-    client.BaseAddress = new Uri("https://cinenicheapi.ngrok-free.app/docs");
+    client.BaseAddress = new Uri("http://localhost:8000");
 });
 builder.Services.AddScoped<MovieRecommenderService>();
 
