b8168fb4140a633529c46d5d7e702a82a1151e50
Nathan Felix <nathan.felix@gmail.com>
2025-04-10 21:20:01 -0600

    publish

--- Backend/CineNiche/CineNiche/obj/Debug/net8.0/CineNiche.AssemblyInfo.cs
+++ Backend/CineNiche/CineNiche/obj/Debug/net8.0/CineNiche.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineNiche")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7152669601b1b3533f9381c8ad8dcc136dab9b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c4759dd0e5bc646cbc71b7be94f465a3612c199")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineNiche")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineNiche")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
